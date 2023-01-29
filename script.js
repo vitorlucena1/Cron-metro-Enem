309acec89753968ee776a60a6782ac22efa8137b
Vitor Campos <vlfc.jogos@gmail.com>
2023-01-28 21:47:21 -0300

    Correção

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "05 Dez 2023";
+const newYears = "05 Nov 2023";
 
 function countdown(){
     const newYearsDate = new Date(newYears);
